8a1d3e9d2dd6b2cdf3e01c60d50d96e72a54d602
rmaga10 <68084540+rmaga10@users.noreply.github.com>
2021-03-26 23:23:32 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ import streamlit as st
 import pandas as pd
 
 st.title('Olá')
-st.write('É nóis')
+st.write('É nóis Truta')
 st.write(pd.DataFrame({
     'primeira': [1,2,3],
     'segunda': [4,5,6]
     
 }))
+
